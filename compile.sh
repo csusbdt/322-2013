10f246cd7f45e8538f019947d8c125bffdda784b
Andrew Brinker <me@andrewbrinker.com>
2013-06-20 12:40:56 -0700

    Added a simple script to generate a book from the markdown files

---
+++ compile.sh
@@ -0,0 +1,7 @@
+if ! type "pandoc" > /dev/null; then
+  echo Please install Pandoc.
+else
+  pandoc -s docs/*.md --latex-engine=xelatex --toc -o book.tex
+  pandoc -s book.tex -o book.pdf
+  rm book.tex
+fi
